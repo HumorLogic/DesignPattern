b5b5e8d1b2de6abc0b64aa42a0f355d216742e55
Humor <humor.ylw@outlook.com>
2020-04-07 15:23:55 +0800

    LKP

---
+++ 迪米特法则/LKP1.cs
@@ -0,0 +1,61 @@
+﻿/// 迪米特法则应用前，在Paperboy类中CollectPayments()方法，包含了
+/// 间接朋友类Wallet的操作，这个操作可以分开写进Customer类中完成
+
+using System.Collections.Generic;
+
+namespace NotUseDesignPattern
+{
+    public class Paperboy
+    {
+        public List<Customer> Customers { get; set; }
+        
+        decimal fundsCollected;
+
+        public Paperboy()
+        {
+            Customers = new List<Customer>();
+        }
+
+
+        public void CollectPayments()
+        {
+            // The newspaper costs $1
+            decimal payment = 1m;
+
+            foreach (Customer customer in Customers)
+            {
+                if (customer.Wallet.Money >= payment)
+                {
+                    customer.Wallet.Money -= payment;
+                    fundsCollected += payment;
+                }
+            }
+        }
+
+
+    }
+
+    public class Customer
+    {
+        public Wallet Wallet { get; set; }
+        public Customer() : this(null) { }
+
+        public Customer(Wallet wallet)
+        {
+            Wallet = wallet;
+        }
+
+
+    }
+
+    public class Wallet
+    {
+        public decimal Money { get; set; }
+        public Wallet(decimal money)
+        {
+            Money = money;
+        }
+
+
+    }
+}
---
+++ 迪米特法则/LKP2.cs
@@ -0,0 +1,71 @@
+﻿///应用迪米特法则
+
+using System.Collections.Generic;
+
+
+namespace DesignPattern
+{
+    public class Paperboy
+    {
+        public List<Customer> Customers { get; set; }
+
+        decimal fundsCollected;
+
+        public Paperboy()
+        {
+            Customers = new List<Customer>();
+        }
+
+
+        public void CollectPayments()
+        {
+            
+            decimal charge= 1m;
+
+            foreach (Customer customer in Customers)
+            {
+                decimal payment = customer.MakePayment(charge);
+
+                if (payment != 0m)
+                {
+                    fundsCollected += payment;
+                }
+            }
+        }
+
+
+    }
+
+    public class Customer
+    {
+        public Wallet Wallet { get; set; }
+        public Customer() : this(null) { }
+
+        public Customer(Wallet wallet)
+        {
+            Wallet = wallet;
+        }
+
+        public decimal MakePayment(decimal amount)
+        {
+            if (Wallet.Money >= amount)
+            {
+                Wallet.Money -= amount;
+                return amount;
+            }
+            return 0m;
+        }
+
+    }
+
+    public class Wallet
+    {
+        public decimal Money { get; set; }
+        public Wallet(decimal money)
+        {
+            Money = money;
+        }
+
+
+    }
+}
